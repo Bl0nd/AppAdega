229780dd0616312f39cf4ebf9325ac8cc6d1fd0e
Bl0nd <fernanda.v.loka@gmail.com>
2025-11-29 00:01:15 -0300

    ...

--- app/views/produto.php
+++ app/views/produto.php
@@ -1,8 +1,8 @@
 <?php require_once('template/head.php') ?>
 
 <body>
-
-<h3>Nossos Produtos</h3>
-
+    <div class="produtos">
+        <h3>Nossos Produtos</h3>
+    </div>
     <?php require_once('template/head.php') ?>
 </body>
