1ead33c43b7cc07a2a8bf850f8feb110497f2f08
Bl0nd <fernanda.v.loka@gmail.com>
2025-12-02 23:05:25 -0300

    ***

--- app/views/produto.php
+++ app/views/produto.php
@@ -4,6 +4,11 @@
     <header></header>
     <div class="produtos">
         <h3>Nossos Produtos</h3>
+
+        <h4>Champagne</h4>
+        <h4>Destilados</h4>
+        <h4>Narguile</h4>
+        <h4>Vinho</h4>
     </div>
 
 
