ef63258395769812ea01c349899efa04ae645b54
Bl0nd <fernanda.v.loka@gmail.com>
2025-12-03 23:07:13 -0300

    .

--- app/views/produto.php
+++ app/views/produto.php
@@ -6,6 +6,16 @@
         <h3>Nossos Produtos</h3>
 
         <h4>Champagne</h4>
+        <div class="champagne">
+            <div class="foto">
+                <img src="#" alt="Foto do Champagne">
+            </div>
+
+            <h2>Nome do Champagne</h2>
+            <p>Marca: <strong>Marca</strong></p>
+
+            <p class="preco">R$ 0,00</p>
+        </div>
         <h4>Destilados</h4>
         <h4>Narguile</h4>
         <h4>Vinho</h4>
