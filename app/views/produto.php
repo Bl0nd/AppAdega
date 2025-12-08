2b1d41e6ca2a84f5f2a491856e5565b799ac5a35
Bl0nd <fernanda.v.loka@gmail.com>
2025-12-08 18:10:15 -0300

    implementação do carrossel individual para cada categoria

--- app/views/produto.php
+++ app/views/produto.php
@@ -3,25 +3,157 @@
 <body>
     <header></header>
     <div class="produtos">
+
         <h3>Nossos Produtos</h3>
 
-        <div class="champagne">
+        <!-- ================= CHAMPAGNE ================= -->
+        <div class="categoria">
             <h4>Champagne</h4>
-            <div class="foto">
-                <img src="#" alt="Foto do Champagne">
+
+            <div class="carrossel">
+                <button class="prev">❮</button>
+
+                <div class="carrossel-wrapper">
+                    <div class="carrossel-items">
+
+                        <div class="item">
+                            <img src="#" alt="">
+                            <h2>Champagne 1</h2>
+                            <p>Marca: <strong>Marca 1</strong></p>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="#" alt="">
+                            <h2>Champagne 2</h2>
+                            <p>Marca: <strong>Marca 2</strong></p>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="#" alt="">
+                            <h2>Champagne 3</h2>
+                            <p>Marca: <strong>Marca 3</strong></p>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="#" alt="">
+                            <h2>Champagne 4</h2>
+                            <p>Marca: <strong>Marca 4</strong></p>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button class="next">❯</button>
             </div>
-
-            <h2>Nome do Champagne</h2>
-            <p>Marca: <strong>Marca</strong></p>
-
-            <p class="preco">R$ 0,00</p>
         </div>
-        
-        <h4>Destilados</h4>
-        <h4>Narguile</h4>
-        <h4>Vinho</h4>
-    </div>
 
+        <!-- ============== DESTILADOS ============== -->
+        <div class="categoria">
+            <h4>Destilados</h4>
+
+            <div class="carrossel">
+                <button class="prev">❮</button>
+
+                <div class="carrossel-wrapper">
+                    <div class="carrossel-items">
+
+                        <div class="item"><img src="#">
+                            <h2>Destilado 1</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Destilado 2</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Destilado 3</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Destilado 4</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button class="next">❯</button>
+            </div>
+        </div>
+
+        <!-- ============== NARGUILE ============== -->
+        <div class="categoria">
+            <h4>Narguile</h4>
+
+            <div class="carrossel">
+                <button class="prev">❮</button>
+
+                <div class="carrossel-wrapper">
+                    <div class="carrossel-items">
+
+                        <div class="item"><img src="#">
+                            <h2>Narguile 1</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Narguile 2</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Narguile 3</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Narguile 4</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button class="next">❯</button>
+            </div>
+        </div>
+
+        <!-- ============== VINHO ============== -->
+        <div class="categoria">
+            <h4>Vinho</h4>
+
+            <div class="carrossel">
+                <button class="prev">❮</button>
+
+                <div class="carrossel-wrapper">
+                    <div class="carrossel-items">
+
+                        <div class="item"><img src="#">
+                            <h2>Vinho 1</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Vinho 2</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Vinho 3</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+                        <div class="item"><img src="#">
+                            <h2>Vinho 4</h2>
+                            <p class="preco">R$ 0,00</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button class="next">❯</button>
+            </div>
+        </div>
+
+    </div>
 
     <?php require_once('template/footer.php') ?>
 </body>
